b40d564917fd4af7580f8602d1ba9e8d3461b5a8
BaskarTV <baskar.tece@gmail.com>
2024-05-03 23:58:17 +0530

    new route added

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello from Flask!'
 
+@app.route('/home')
+def home():
+    return 'Home page'
+# GitAction WorkFlow is successfull working
+
 #app.run(port=5000, debug=True)
